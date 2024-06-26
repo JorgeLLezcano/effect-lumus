78f16cacdf00b469443d6492b5f5aa41147189f8
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2024-06-26 02:08:53 +0000

    cambio de front

--- .idx/magic-web-HP/script.js
+++ .idx/magic-web-HP/script.js
@@ -23,46 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
     .then(response => response.json())
     .then(data => {
       data.forEach(character => {
-
-        // const cardElement = document.createElement('div');
-        // cardElement.classList.add('card');
-        
-        // const frontElement = document.createElement('div');
-        // frontElement.classList.add('front');
-        
-        // const h1Element = document.createElement('h1');
-        // h1Element.textContent = character.name;
-        // frontElement.appendChild(h1Element);
-        
-        // const imgElement = document.createElement('img');
-        // imgElement.src = character.image;
-        // imgElement.alt = character.name;
-        // cardElement.appendChild(imgElement);
-  
-        // const infoElement = document.createElement('div');
-        // infoElement.classList.add('card-info');
-  
-        // const nameElement = document.createElement('h3');
-        // nameElement.textContent = character.name;
-        // infoElement.appendChild(nameElement);
-  
-        // const houseElement = document.createElement('p');
-        // houseElement.textContent = `House: ${character.house}`;
-        // infoElement.appendChild(houseElement);
-  
-        // const patronusElement = document.createElement('p');
-        // patronusElement.textContent = `Patronus: ${character.patronus}`;
-        // infoElement.appendChild(patronusElement);
-  
-        // cardElement.appendChild(infoElement);
-        // container.appendChild(cardElement);
-        // cardElement.appendChild(frontElement);
-
-        
         personajes=`
         <div class="card">
             <div class="front">
               <h1>${character.name}</h1>
+              <img src="${url}">
             </div>
             <div class="back">
               <img src="${character.image}">
