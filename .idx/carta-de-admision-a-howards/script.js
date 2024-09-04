cb5572543a053892e13ab8078f198768ea7f7297
jorgeluislezcano1988 <jorgeluislezcano1988@gmail.com>
2024-09-04 02:31:47 +0000

    nuevo proyecto con api de dragon ball

--- .idx/carta-de-admision-a-howards/script.js
+++ .idx/carta-de-admision-a-howards/script.js
@@ -39,7 +39,45 @@ fetch(API)
           <div class="front">
             <h1>${character.name}</h1>
             <img src="${character.image || imagenPorDefecto}">
-            
+           
+          </div>
+          <div class='info'>
+          <p>
+          ${character.patronus ? `Patronus: ${character.patronus}` : ''}
+          </p>
+          <p>
+          ${character.house ? `House: ${character.house}` : ''}
+          </p>
+          <p>
+          ${character.species ? `Species: ${character.species}` : ''}
+          </p>
+          <p>
+          ${character.gender ? `Gender: ${character.gender}` : ''}
+          </p>
+          <p>
+          ${character.dateOfBirth ? `Date of Birth: ${character.dateOfBirth}` : ''}
+          </p>
+          <p>
+          ${character.ancestry ? `Ancestry: ${character.ancestry}` : ''}
+          </p>
+          <p>
+          ${character.eyeColour ? `Eye Colour: ${character.eyeColour}` : ''}
+          </p>
+          <p>
+          ${character.hairColour ? `Hair Colour: ${character.hairColour}` : ''}
+          </p>
+          <p>
+          ${character.actor ? `Actor: ${character.actor}` : ''}
+          </p>
+          <p>
+          ${character.alive ? `Alive: ${character.alive}` : ''}
+          </p>
+          <p>
+          ${character.wand ? `Wand: ${character.wand.wood} ${character.wand.core} ${character.wand.length}` : ''}
+          </p>
+          <p>
+          ${character.wizard ? `Wizard: ${character.wizard}` : ''}
+          </p>
           </div>
         </div>
    
